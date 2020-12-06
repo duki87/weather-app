02783fad638c61c2aa9c3ad6ed928ed7277cd5e1
Dusan Marinkovic <dusanmarinkovic@hotmail.com>
2020-12-06 23:31:18 +0100

    added custom angular material theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './includes/nav/nav.component';
 
 //Material
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatButtonModule } from '@angular/material/button'; 
 import { MatAutocompleteModule } from '@angular/material/autocomplete'; 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSidenavModule } from '@angular/material/sidenav';  
+import { MatListModule } from '@angular/material/list';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -28,10 +30,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule,
     //Material
+    FlexLayoutModule,
     MatButtonModule,
     MatAutocompleteModule,
     MatFormFieldModule,
     MatSidenavModule,
+    MatListModule,
 
   ],
   providers: [],
